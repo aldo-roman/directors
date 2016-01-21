937577006c9548f8980519603f0a104ce50cc547
Aldo Román Nureña <aldo.roman.nurena@gmail.com>
2016-01-21 14:32:00 -0500

    removed js and js.map files

--- src/app/index.config.js
+++
@@ -1,9 +0,0 @@
-/** @ngInject */
-function config($logProvider, $mdThemingProvider) {
-    // enable log
-    $logProvider.debugEnabled(true);
-    $mdThemingProvider.theme('default')
-        .primaryPalette('blue');
-}
-exports.config = config;
-//# sourceMappingURL=index.config.js.map
